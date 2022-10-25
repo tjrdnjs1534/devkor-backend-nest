cade942630cc53ddea26acb0a97cbe9176a6e8c9
SeokWon <tjrdnjs1534@naver.com>
2022-10-25 19:33:00 +0900

    feat: Postgres db connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot(typeORMConfig),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/users.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn} from "typeorm"
+
+@Entity()
+export class UserEntity{
+    @PrimaryGeneratedColumn()
+    id : number;
+    
+    @Column()
+    name : string;
+    
+    @Column()
+    age :number;
+
+    @Column()
+    role : number;
+}
