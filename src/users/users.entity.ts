7fab18f3089fc1bfae79ba09036446ed133f6163
SeokWon <tjrdnjs1534@naver.com>
2022-11-03 10:04:07 +0900

    feat: user service update

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -2,7 +2,7 @@ import { Column, Entity, PrimaryGeneratedColumn} from "typeorm"
 
 @Entity()
 export class UserEntity{
-    @PrimaryGeneratedColumn()
+    @PrimaryGeneratedColumn() // 지워도 초기화 안되는듯?? 
     id : number;
     
     @Column()
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -12,45 +12,45 @@ export class UsersService {
     private usersRepository: Repository<UserEntity>,
   ) {}
 
-  getAllUsers() : Promise<UserEntity[]> {
+  async getAllUsers(): Promise<UserEntity[]> {
     return this.usersRepository.find();
   }
 
-  async getUserByID(id : number) : Promise<UserEntity> {
-    const user = await this.usersRepository.findOneBy({ id })
-    if(!user) {
-        throw new NotFoundException("can't find user");
+  async getUserByID(id: number): Promise<UserEntity> {
+    const user = await this.usersRepository.findOneBy({ id });
+    if (!user) {
+      throw new NotFoundException("can't find user");
     }
-    return user
+    return user;
   }
 
-  async createUser(createUserDto : CreateUserDto) {
-    const {id, name, age, role} = createUserDto;
-    const newUser = this.usersRepository.create({
-        id,
-        name,
-        age,
-        role
-    });
-    await this.usersRepository.insert(newUser);
-    return newUser  
+  async createUser(createUserDto: CreateUserDto) {
+    // const { id, name, age, role } = createUserDto;
+    // const newUser = this.usersRepository.create({
+    //   id,
+    //   name,
+    //   age,
+    //   role,
+    // });
+    await this.usersRepository.save(createUserDto);
+    return createUserDto;
   }
 
-  async deleteUser(id : number) : Promise<void> {
+  async deleteUser(id: number): Promise<void> {
     await this.usersRepository.delete(id);
   }
 
-  async updateUser(id: number, updateUserDto : UpdateUserDto) : Promise<void> {
-    const updateUser = await this.usersRepository.findOneBy({ id })
-    if(!updateUser){
+  async updateUser(id: number, updateUserDto: UpdateUserDto): Promise<void> {
+    const updateUser = await this.usersRepository.findOneBy({ id });
+    if (!updateUser) {
       throw new NotFoundException("can't find user");
     }
-    const {age, role} = updateUserDto
-    await this.usersRepository.update(
-      updateUser,
-      {age,role}
-    );
-     //update와 save 차이, id와 name은 변경할 수 없음
+    await this.usersRepository.update(id, {
+      age: updateUserDto.age,
+      role: updateUserDto.role,
+    });
+
+    //update와 save 차이, id와 name은 변경할 수 없음
     //this.USER_DUMMY.push({...updateUser, ...updateUserDto})
   }
 }
