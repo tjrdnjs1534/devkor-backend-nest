9fc54bdb4990852e53885e3280898548a36c7eed
SeokWon <tjrdnjs1534@naver.com>
2022-11-28 19:00:07 +0900

    feat: update category many to many relation

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -22,16 +22,13 @@ export class ProductsService {
   ) {}
 
   async addPhoto(id: number, createPhotoDto: CreatePhotoDto) {
-    // await this.photoRepository.save(createPhotoDto);
-    // const product_photo = new PhotoEntity();
-    // product_photo.url = createPhotoDto.url;
     const product = await this.productsRepository.findOneBy({ id });
     for (const url of createPhotoDto.url) {
       await this.photoRepository.save({ url: url, product: product });
     } // url array로 바꿈
   }
 
-  async removePhoto(id: number) {
+  async removeAllPhoto(id: number) {
     const productID = id;
     await this.photoRepository
       .createQueryBuilder()
@@ -40,6 +37,30 @@ export class ProductsService {
       .where('ref_productID = :ref_productID', { ref_productID: productID })
       .execute();
   } // photo id 받아서 ? 고민 필요
+
+  async addCategory(id: number, updateProductDto : UpdateProductDto) {
+    const updateProduct = await this.productsRepository.findOne({
+      where: { id: id },
+      relations: ['photos', 'categories'],
+    });
+    for (const category of updateProductDto.categories) {
+      const findcategory = await this.categoryRepository.findOne({
+        where: { category_name: category },
+        relations: ['product'],
+      });
+      findcategory.product.push(updateProduct);
+      await this.categoryRepository.save(findcategory);
+    }
+  }
+
+  async removeAllCategory(id: number,) {
+    const updateProduct = await this.productsRepository.findOne({
+      where: { id: id },
+      relations: ['categories'],
+    });
+    updateProduct.categories = []
+    await this.productsRepository.save(updateProduct);
+  }
 
   async createProduct(createProductDto: CreateProductDto) {
     const product = new ProductEntity();
@@ -52,7 +73,6 @@ export class ProductsService {
         where: { category_name: category },
         relations: ['product'],
       });
-
       findcategory.product.push(product);
       await this.categoryRepository.save(findcategory);
     } // 기존 카테고리에 등록하도록 함, error처리 필요
@@ -97,25 +117,14 @@ export class ProductsService {
       price: updateProductDto.price,
       //categories : updateProductDto.
     });
-    this.removePhoto(id);
-    this.addPhoto(id, updateProductDto.photos);
-    // const updatePhoto = []
-    // for (const url of updateProductDto.photos.url) {
-    //   const photo = new PhotoEntity()
-    //   photo.url = url;
-    //   photo.product = updateProduct
-    //   console.log(photo)
-    //   await this.photoRepository.save(photo);
-    //   //updatePhoto.push(photo);
-    // }
-    // const ref_productID = id;
-    // await this.productsRepository.createQueryBuilder().update(ProductEntity).set({name: updateProductDto.name,
-    //   price: updateProductDto.price, }).where("id = :id", {id:}).execute()
-    // //
-    // await this.productsRepository.update(id, {
-    //   photos: updatePhoto,
-    // });
-  } // 사진 업데이트는 다 지우고 새로 추가하는 방식, 카테고리 업데이트도 -- 지우고 새로 처음부터?
+    await this.removeAllPhoto(id);
+    await this.addPhoto(id, updateProductDto.photos);
+    await this.removeAllCategory(id);
+    await this.addCategory(id,updateProductDto);  
+
+  } // 사진 카테고리  다 지우고 새로 추가하는 방식, 더 깔끔하게 할 방법찾기
+  // update product를 direct로 넘겨서 추가적인 where 문 없도록 하기
+  //Cannot query across one to many, many-to-many for property categories
 
   async removeProduct(id: number) {
     await this.productsRepository.delete(id); // 삭제 시 사진 정보도 같이 삭제 되게 함, 카테고리는 남아있음
