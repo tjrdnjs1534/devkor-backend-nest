0d0236fe4f61fa6e1eaaad23542f9d015472b4f9
SeokWon <tjrdnjs1534@naver.com>
2022-12-27 16:12:24 +0900

    feat: payload change

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -21,7 +21,7 @@ export class AuthService {
   }
 
   async login(user: any) {
-    const payload = { name: user.name, role: user.role };
+    const payload = { name: user.name, role: user.role, id:user.id };
     return {
       access_token: this.jwtService.sign(payload),
     };
